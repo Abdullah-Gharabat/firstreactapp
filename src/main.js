381872ec51637d6350f3de45ca3e35607bd30f28
Abdullah-Gharabat <abdallahghrabat@gmail.com>
2023-09-28 13:23:22 +0300

    Edit

--- src/main.js
+++ src/main.js
@@ -1,21 +1,34 @@
 import data from "./data.json";
 import CardCopm from "./card";
+import "./main.css";
+import { useState } from "react";
 
 function Main() {
+  const [items, setItems] = useState(data);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let searchedValue = event.target.search.value.toLowerCase();
+
+    // Filter items based on the searched value
+    let filteredItems = data.filter(function (item) {
+      return item.title.toLowerCase().includes(searchedValue);
+    });
+
+    setItems(filteredItems);
+  }
+
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "space-between",
-          gap: "20px",
-          marginTop: "3%",
-        }}
-      >
-        {data.map(function (item) {
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="search" />
+        <button type="submit">Search</button>
+      </form>
+      <div id="container">
+        {items.map(function (item) {
           return (
             <CardCopm
+              key={item.title}
               image={item.image_url}
               title={item.title}
               description={item.description}
